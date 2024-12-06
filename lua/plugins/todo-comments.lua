96ac1f207d222eb8b2826276f7b9233f54beb56d
Sawyer Watts <sawyer.watts@gmail.com>
2024-12-06 14:12:56 -0800

    Add VULN and BLOCKED todo comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,5 +1,16 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+      keywords = {
+        VULN = { icon = ' ', color = 'error', alt = { 'VULNERABILITY', 'VULNERABLE' } },
+        BLOCKED = { icon = ' ' },
+      },
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
