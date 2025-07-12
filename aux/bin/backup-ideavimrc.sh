6d47db1506a2e38877c327f9ce0667f86af3b983
Sawyer Watts <sawyer.watts@gmail.com>
2025-07-12 08:54:09 -0700

    write backup-ideavimrc

---
+++ aux/bin/backup-ideavimrc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+# Note that `set +e` is the syntax to disables variable strictness. This is
+# particularly helpful if you need to source a script that violates any of these
+# `set`s.
+
+nvim_root=~/.config/nvim
+save_path=$nvim_root/aux/homeconfigs/
+
+cp ~/.ideavimrc "$save_path"
+cd "$nvim_root"
+git add "$save_path"
+git commit -m "Update ideavimrc"
+git pull
+git push
+
